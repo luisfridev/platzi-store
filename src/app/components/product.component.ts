9bb666ef95c51f408e2180979b533f9c811e5da7
Luis <lfriasherrera@gmail.com>
2019-10-30 17:47:57 -0500

    Estilos para mostrar la lista de productos

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -4,7 +4,8 @@ import { Product } from '../models/product';
 
 @Component({
   selector: 'app-product',
-  templateUrl: './product.component.html'
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
 })
 
 export class ProductComponent {
