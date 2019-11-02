b2fd5692eac109a24d3037c91cecc534f420a291
Luis <lfriasherrera@gmail.com>
2019-11-02 11:08:48 -0500

    Prepara el proyecto para Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,10 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product.component';
 import { ExponentialPipe } from './exponential.pipe';
 import { HighlightDirective } from './highlight.directive';
-import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
 import { HeaderComponent } from './header/header.component';
-import { BannerComponent } from './banner/banner.component';
 import { FooterComponent } from './footer/footer.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
@@ -24,12 +22,10 @@ import { LayoutComponent } from './layout/layout.component';
     ProductComponent,
     ExponentialPipe,
     HighlightDirective,
-    HomeComponent,
     ProductsComponent,
     ContactComponent,
     DemoComponent,
     HeaderComponent,
-    BannerComponent,
     FooterComponent,
     PageNotFoundComponent,
     ProductDetailComponent,
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+
+import { BannerComponent } from './components/banner/banner.component';
+import { HomeComponent } from './home.component';
+
+import { HomeRoutingModule } from './home-routing.module';
+
+@NgModule({
+  declarations: [
+    BannerComponent,
+    HomeComponent
+  ],
+  imports: [
+    HomeRoutingModule
+  ],
+  exports: [],
+  providers: [],
+})
+
+export class HomeModule {
+}
